6c5e6d95480d9cc3e9a5a096cb9a4ccfb4c8627a
naufalwdd <mnwidad23@gmail.com>
2025-04-09 10:47:05 +0700

    fix lagi

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        '': ['templates/**/*']  # menyertakan semua file dalam templates/
+    },
     entry_points={
         'console_scripts': [
             'flaskbuilder=flaskbuilder.__main__:main',
         ],
     },
-    install_requires=[
-        'click',
-    ],
 )
